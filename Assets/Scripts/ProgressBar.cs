bc203f57c6ed40aca0b72a64ba16eec936b86ecd
Kevin Celinski <kevin.celinski@outlook.com>
2020-07-01 15:26:47 +0200

    :sparkles: Custom Progress Bars (#29)
    *Radial Bar
    *Linear Bar

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+namespace Assets.Scripts
+{
+    [ExecuteInEditMode]
+    public class ProgressBar : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        [MenuItem("GameObject/UI/Linear Progress Bar")]
+        public static void AddLinearProgressBar()
+        {
+            GameObject go = Instantiate(Resources.Load<GameObject>("UI/Linear Progress Bar"));
+            go.transform.SetParent(Selection.activeGameObject.transform, false);
+        }
+        [MenuItem("GameObject/UI/Radial Progress Bar")]
+        public static void AddRadialProgressBar()
+        {
+            GameObject go = Instantiate(Resources.Load<GameObject>("UI/Radial Progress Bar"));
+            go.transform.SetParent(Selection.activeGameObject.transform, false);
+        }
+#endif
+
+
+
+        public int Minimum;
+        public int Maximum;
+        public int Current;
+        public Image Mask;
+        public Image Fill;
+        [SerializeField] private Color _color = Color.white;
+
+        // Update is called once per frame
+        void Update()
+        {
+            GetCurrentFill();
+        }
+
+        void GetCurrentFill()
+        {
+            float currentOffset = Current - Minimum;
+            float maximumOffset = Maximum - Minimum;
+            float fillAmount = currentOffset / maximumOffset;
+            Mask.fillAmount = fillAmount;
+            Fill.color = _color;
+        }
+    }
+}
